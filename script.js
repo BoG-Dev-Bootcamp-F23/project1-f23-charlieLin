bb90ac2e22704219e822186351a6e525f996c5d5
cjlin123 <108144107+cjlin123@users.noreply.github.com>
2023-10-02 14:09:04 -0400

    created dictionary/map for 'type : hexcode'

--- script.js
+++ script.js
@@ -9,10 +9,33 @@
 let dexNumber = 1;
 let pokemonObject = `https://pokeapi.co/api/v2/pokemon/${dexNumber}/`;
 
-let pokeSprite = document.querySelector("#pokemon-sprite");
-let pokeName = document.querySelector("#pokemon-name");
+const pokeSprite = document.querySelector("#pokemon-sprite");
+const pokeName = document.querySelector("#pokemon-name");
 const leftButton = document.querySelector("#left-button");
 const rightButton = document.querySelector("#right-button");
+const firstType = document.querySelector("#type-1");
+const secondType = document.querySelector("#type-2");
+const typeColors = {
+    "normal" : "#A8A77A",
+    "fire" : "#EE8130",
+    "water" : "#6390F0",
+    "electric" : "#F7D02C",
+    "grass" : "#7AC74C",
+    "ice" : "#96D9D6",
+    "fighting" : "#C22E28",
+    "poison" : "#A33EA1",
+    "ground" : "#E2BF65",
+    "flying" : "#A98FF3",
+    "psychic" : "#F95587",
+    "bug" : "#A6B91A",
+    "rock" : "#B6A136",
+    "ghost" : "#735797",
+    "dragon" : "#6F35FC",
+    "dark" : "#705746",
+    "steel" : "#B7B7CE",
+    "fairy" : "#D685AD",
+}
+
 
 const pokemonResponse = async () => {
   try {
@@ -64,11 +87,19 @@ const updateName = async () => {
       }
 };
 
-const updateType = () => {};
+const updateType = async () => {
+    try {
+        const pokemonData = await pokemonResponse();
+        const pokemonTypeDataArr = pokemonData.types
+      } catch (error) {
+        console.log(error);
+      }
+};
 
 //Initial call for loading
 updateSprite();
 updateName();
+updateType();
 
 leftButton.addEventListener("click", () => {
   decrementDex();
